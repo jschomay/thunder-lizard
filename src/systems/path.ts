35991e80f28ec7bf439a7e20bdd4a8a0228f6cbb
Jeff Schomay <1825491+jschomay@users.noreply.github.com>
2025-05-18 11:38:35 -0400

    fix path finding bug

--- src/systems/path.ts
+++ src/systems/path.ts
@@ -9,12 +9,12 @@ function init(eid: number) {
 
 function length(eid: number) {
   let l = Pursue.path[eid].indexOf(-1)
-  return l === -1 ? 0 : l / 2
+  return l === -1 ? 0 : (l - Pursue.offset[eid]) / 2
 }
 
 function push(eid: number, x: number, y: number) {
   let path = Pursue.path[eid]
-  let endOfPath = length(eid) * 2
+  let endOfPath = Pursue.path[eid].indexOf(-1)
   path[endOfPath] = x
   path[endOfPath + 1] = y
 }
--- tests/path.test.ts
+++ tests/path.test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from 'vitest'
 import Path from '../src/systems/path'
+import { Pursue } from '../src/components'
 
 
 test('path', () => {
@@ -10,9 +11,24 @@ test('path', () => {
 
   Path.push(eid, 0, 0)
   Path.push(eid, 0, 1)
-  expect(Path.length(eid)).toBe(2)
+  Path.push(eid, 1, 1)
+  expect(Path.length(eid)).toBe(3)
   expect(Path.current(eid)).toEqual([0, 0])
 
   Path.advance(eid)
+  expect(Path.length(eid)).toBe(2)
   expect(Path.current(eid)).toEqual([0, 1])
+  Path.advance(eid)
+  expect(Path.length(eid)).toBe(1)
+  expect(Path.current(eid)).toEqual([1, 1])
+
+  // diagonals
+  Path.push(eid, 2, 2)
+  expect(Path.length(eid)).toBe(2)
+  expect(Path.current(eid)).toEqual([1, 1])
+
+  // reset
+  Path.init(eid)
+  expect(Path.length(eid)).toBe(0)
+  expect(Pursue.offset[eid]).toBe(0)
 })
